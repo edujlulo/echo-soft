12c2153fb87be5a58429aac4b4d7ce58e81820e9
Eduardo Lulo <eduardo.lulo.m@gmail.com>
2026-03-31 13:20:22 +0100

    Create DynamicTextarea file

---
+++ app/consultations/quick-mode/_components/DynamicTextarea.tsx
@@ -0,0 +1,9 @@
+import ConsultLabeledTextarea from "@/components/ConsultLabeledTextarea";
+
+export default function DynamicTextarea() {
+  return (
+    <div>
+      <ConsultLabeledTextarea>MOTIVOS</ConsultLabeledTextarea>
+    </div>
+  );
+}
