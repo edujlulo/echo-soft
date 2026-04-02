87e9bcdc0faa1a695f112f48d6baa1862e9a874c
Eduardo Lulo <eduardo.lulo.m@gmail.com>
2026-04-02 10:35:50 +0100

    Feat(ReportDraft): add readOnly insted of disabled

--- app/consultations/tabs/summary-report-tab/ReportDraft.tsx
+++ app/consultations/tabs/summary-report-tab/ReportDraft.tsx
@@ -12,7 +12,7 @@ export default function ReportDraft() {
 
         <textarea
           value={report}
-          disabled
+          readOnly
           className="w-[700px] h-[560px] bg-white border border-blue-200 px-2 pb-0.5 pt-1.5 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-500 focus:bg-white"
         />
       </div>
